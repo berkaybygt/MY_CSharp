c511872d53bf1b8d3d19b3769dd3884760e0f3a0
BB <BB@DESKTOP-1H4SN6F>
2024-10-04 19:13:13 +0300

    Add project files.

---
+++ A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayi = { 1, 2, 3, 4, 5 };
+
+            foreach (int i in sayi)
+            {
+                Console.WriteLine(sayi);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sayi1, sayi2, sonuc;
+
+            Console.Write("İlk Sayı: ");
+            sayi1 = double.Parse(Console.ReadLine());
+
+            Console.Write("İkinci Sayı: ");
+            sayi2 = double.Parse(Console.ReadLine());
+
+
+            if (sayi1 < 0 || sayi2 < 0)
+            {
+                Console.WriteLine("Pozitif Sayi Girin.");
+            }
+            else
+            {
+                sonuc = sayi1 + sayi2;
+                Console.WriteLine(sonuc);
+
+                Console.Read();
+            }
+        }
+    }
+}
