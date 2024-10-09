15a6c9d601359bbffa4569d81288251a8f985933
BB <BB@DESKTOP-1H4SN6F>
2024-10-09 18:18:38 +0300

    up

---
+++ If_Switch/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region If-Else
+            #region Soru1
+            //Console.Write("Sayi Gir: ");
+            //int inp = int.Parse(Console.ReadLine());
+
+            //if (inp == 0)
+            //{
+            //    Console.WriteLine("Sayi Sifir");
+            //}
+            //else if (inp % 2 == 0)
+            //{
+            //    Console.WriteLine("Çift");
+            //}
+            //else {
+            //    Console.WriteLine("Tek");
+            //}
+            #endregion
+
+            #region Soru2
+
+            //Console.WriteLine("İlk Sayi:");
+            //int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("İkinci Sayi:");
+            //int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            //if (sayi1 > sayi2)
+            //{
+            //    Console.WriteLine($"{sayi1} büyük");
+            //}
+            //else if (sayi2 > sayi1)
+            //{
+            //    Console.WriteLine($"{sayi2} büyük");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Eşit");
+            //}
+            #endregion
+            #endregion
+
+            #region Switch
+
+            //Console.Write("Gir: ");
+            //int ay = int.Parse(Console.ReadLine());
+
+            //switch (ay)
+            //{
+            //    case 1: Console.WriteLine("Ocak"); break;
+            //    case 2: Console.WriteLine("Şubat"); break;
+            //    case 3: Console.WriteLine("Mart"); break;
+            //    case 4: Console.WriteLine("Nisan"); break;
+            //    case 5: Console.WriteLine("Mayis"); break;
+            //    default: Console.WriteLine("Hata"); break;
+            //}
+            #endregion
+        }
+    }
+}
